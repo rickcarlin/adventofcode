8537ce8c1fa8cba8a806908d2fb248386f11b873
rickcarlin <rickcarlin77@gmail.com>
2022-05-26 18:25:19 -0400

    rename day.sh day1.sh

---
+++ day-1/rust/target/debug/deps/rust-7de92f4249427cc2.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/rickc/Documents/MyAdventOfCode/adventofcode/day-1/rust/target/debug/deps/rust-7de92f4249427cc2: src/main.rs
+
+/mnt/c/Users/rickc/Documents/MyAdventOfCode/adventofcode/day-1/rust/target/debug/deps/rust-7de92f4249427cc2.d: src/main.rs
+
+src/main.rs:
---
+++ day-1/rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/rickc/Documents/MyAdventOfCode/adventofcode/day-1/rust/target/debug/rust: /mnt/c/Users/rickc/Documents/MyAdventOfCode/adventofcode/day-1/rust/src/main.rs
