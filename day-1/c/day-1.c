55cda15a7a1b2a9b3f48ebdbf27dccb381771dc0
rickcarlin <rickcarlin77@gmail.com>
2022-05-27 10:53:03 -0400

    C code

---
+++ day-1/c/day-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main( int argc, char *argv[] ) {
+
+    // Assumes first cmdline param will be input filename. If not passed, print usage and exit
+    if (argc < 2) {
+        
+    }
+}
